eef53b8690bcc85021a1b0fe8df853eaf85de7e4
albandewilde <dewilde@intechinfo.fr>
2019-11-02 03:46:11 +0100

    The `.gitkeep` is not send

--- get_file.py
+++ get_file.py
@@ -7,6 +7,7 @@ def rdm(folder_path):
     """Return a random file name in the given folder"""
     path = os.path.abspath(folder_path)
     files = os.listdir(path)
+    files.remove(".gitkeep")    # remove this file to get only images
     file_name = files[randrange(len(files))]
     return file_name
 
